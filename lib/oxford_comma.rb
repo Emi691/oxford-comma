66690e6cbd1e1dc677aaecdc413ab792b10b9bb8
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-01-13 11:37:31 -0800

    commit tree a42c5e8716e7eb79594c40616ffe65527abb6aa2

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join(",")
+    elsif array.length == 2
+        array.join(" and ")
+    else 
+        last = array[-1]
+        array.pop
+        array << ("and #{last}")
+        array.join(", ")
+    end    
 end
